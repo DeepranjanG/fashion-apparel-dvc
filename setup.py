426bc3a853ca3f24aecbb43694bf0ec41b9e55d9
DeepranjanG <kdeepu070@gmail.com>
2021-10-04 12:43:54 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    description="It's a fashion Apparel Package",
+    author="DeepranjanG",
+    packages=find_packages(),
+    licence="MIT"
+)
